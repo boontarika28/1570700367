70be5748ff3bef717380a592f2dd25e710922f01
DELL50\DELL81 <boontarika.sunl@bumail.net>
2017-02-23 20:04:34 +0700

    11.game controller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,12 +7,13 @@ public class GameController : MonoBehaviour {
 	public GameObject hazard;
 	public Vector3 spawnValues;
 
-	void start () {
+	void Start () {
 		SpawnWaver ();
 	}
 
-	void SpawnWaver (){
-		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x,spawnValues.x), spawnValues.y, spawnValues.z);
+
+	void SpawnWaver () {
+		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 		Quaternion spawnRotation = Quaternion.identity;
 		Instantiate (hazard, spawnPosition, spawnRotation);
 	}
