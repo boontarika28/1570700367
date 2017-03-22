13a0e2f71dd40a7f3fcb980f9c471108535bf9fc
DELL50\DELL81 <boontarika.sunl@bumail.net>
2017-03-22 23:25:11 +0700

    14.Couting poit and display

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,7 +11,12 @@ public class GameController : MonoBehaviour {
 	public float startWait;
 	public float waveWait;
 
+	public GUIText scorerText;
+	private int score;
+
 	void Start () {
+		score = 0;
+		UpdateScore ();
 		StartCoroutine (SpawnWaver ());
 	}
 
@@ -31,4 +36,15 @@ public class GameController : MonoBehaviour {
 			yield return new WaitForSeconds (waveWait);
 		}
 	}
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore()
+	{
+		scorerText.text = "Score: " + score;
+	}
+
 }
